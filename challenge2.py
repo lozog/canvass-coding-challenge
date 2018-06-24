07ffe9cc45e1367bfc5ac5f146fe7a09f34589c9
lozog <liam.ozog@gmail.com>
2018-06-23 22:09:08 -0400

    challenge 2: add support for large files

--- challenge2.py
+++ challenge2.py
@@ -8,14 +8,17 @@ def datesortkey(L):
 	return int(split[2]),int(split[0]),int(split[1]) 
 
 with open('random_data.csv') as csvfile:
-	readCSV = csv.DictReader(csvfile, delimiter=',')
 
-	# sort by Device_ID, then by Date
-	sortedlist = sorted(readCSV, key=lambda row: (row['Device_ID'], datesortkey(row['Date'])), reverse=False)
+	# in order to handle large files: read in first, sort later
+	data = []
+	for row in csv.DictReader(csvfile, delimiter=','):
+		data.append(row)
+
+# sort by Device_ID, then Date
+data.sort(key=lambda row: (row['Device_ID'], datesortkey(row['Date'])), reverse=False)
 
 fieldnames = ['Device_ID', 'A', 'B', 'C', 'D', 'Date'] # hard-coded field names
 with open('random_data_sorted.csv', 'w') as f:
 	writer = csv.DictWriter(f, fieldnames=fieldnames)
 	writer.writeheader()
-	for row in sortedlist:
-		writer.writerow(row)
+	writer.writerows(data)
